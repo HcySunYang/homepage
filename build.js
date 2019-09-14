24a5505f6817a8f5de7d9b27a14d3955917a63d6
HcySunYang <HcySunYang@outlook.com>
2019-09-14 17:04:43 +0800

    My Anime List

--- build.js
+++ build.js
@@ -1,14 +1,20 @@
 const minify = require('html-minifier').minify
 const fs = require('fs')
 
-const source = fs.readFileSync('./index-src.html', 'utf-8')
-const result = minify(source, {
-  collapseWhitespace: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  useShortDoctype: true,
-  minifyCSS: true
-})
+function doMinify(paths) {
+  paths.forEach(p => {
+    const source = fs.readFileSync(p, 'utf-8')
+    const result = minify(source, {
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      useShortDoctype: true,
+      minifyCSS: true
+    })
 
-fs.writeFileSync('./index.html', result, 'utf-8')
+    fs.writeFileSync(p.replace('-src', ''), result, 'utf-8')
+  })
+}
+
+doMinify(['./index-src.html', './anime-list-src.html'])

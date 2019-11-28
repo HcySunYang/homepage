7bcb2bdb8af2e9ac10d18c8e36515021f20908a6
HcySunYang <HcySunYang@outlook.com>
2019-11-28 18:40:58 +0800

    notify

---
+++ notify.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+if (!process.argv[2]) return
+
+axios
+  .post(
+    'https://qyapi.weixin.qq.com/cgi-bin/webhook/send',
+    {
+      msgtype: 'text',
+      text: {
+        content: '测试2'
+      }
+    },
+    {
+      params: {
+        key: process.argv[2]
+      },
+      headers: { 'Content-Type': 'application/json' }
+    }
+  )
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
